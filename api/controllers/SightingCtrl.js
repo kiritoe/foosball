7e6e1294551015c6b92443fd2630c123a40e37a0
Mackay Nicolson <mac.nicolson3@gmail.com>
2015-08-25 15:08:38 -0600

    udate

---
+++ api/controllers/SightingCtrl.js
@@ -0,0 +1,1 @@
+var Sighting = require('../models/Sighting');
--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 //DEPNDENCIES
-var express = require('express')
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+//SERVER
+var app = express();
+var data = {'message': 'did you get this'};
+
+//mongoDB connection
+var port = 8080
+var mongoUri = 'mongodb://localhost:27017/foosball'
+
+mongoose.set('debug', true);
+mongoose.connect(mongoUri);
+mongoose.connection.once('open', function() {
+   console.log('MongoDB cpnnected at: ', mongoUri);
+});
+
+
+
+//ENDPOINTS
+
+app.listen(port, function() {
+	console.log('do you see me',port);
+});
